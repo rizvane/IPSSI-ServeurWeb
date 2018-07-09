189ecc42e65ebad7ac7d66c80add2a4f0bdda3c1
rizvane <36622818+rizvane@users.noreply.github.com>
2018-07-09 23:31:11 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,1 +1,13 @@
-<?php echo "lol" ;?>
+<?php
+    $title = 'Site d\'exemple 1';
+?>
+<!Doctype>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?= $title ?></title>
+    </head>
+    <body>
+        <h1><?= $title ?></h1>
+    </body>
+</html>
